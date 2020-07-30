4b28b14927897f8b1f2a5ae8d3e1447079097fb1
AydanPirani <aydanpirani@gmail.com>
2020-07-29 22:53:54 -0700

    Reverted changes

--- script.py
+++ script.py
@@ -4,17 +4,25 @@ dir = "STL"
 
 i = 1
 extension = ".stl"
-template = "electrode"
+template = "updated_electrode"
 
 script_path = os.path.realpath(__file__)
 dir_path = os.path.dirname(script_path) + "\\" + dir
 files = os.listdir(dir_path)
 
+if files[0][0:9] != "electrode":
+    template = "electrode"
+
 os.chdir(dir_path)
 
 for file in files:
-    new_name = template + str(i) + extension
-    os.rename(file, new_name)
-    print("renamed " + file)
+    if file[-4:] == ".stl":
+        new_name = template + str(i) + extension
+        os.rename(file, new_name)
+        print("renamed " + file)
+    else:
+        print("kept " + file)
     i = i + 1
-print("Done!")
+
+print()
+print("done! renamed " + str(i-1) + " files")
